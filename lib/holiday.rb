777beb3b0f0777b86f33f313afacbb6cb6849d49
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 03:45:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday_hash.each do |holiday, supplies|
       supplies.find do |element|
-        bbq_days.push(holiday)
+        bbq_days.push(supplies)
       end
     end
   end
