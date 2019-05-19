7fed5be3e708a01dbe125f9e685f0846137b93cb
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 03:40:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday_hash.each do |holiday, supplies|
       supplies.each do |thing|
-        if holiday.values.include?("BBQ")
+      if holiday.values.include?("BBQ")
         bbq_days << holiday
       end
     end
